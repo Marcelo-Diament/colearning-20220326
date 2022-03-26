1a2cd2ecc501c31339939d2725683c84f282d118
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-26 11:56:05 -0300

    feat: tirando proveito do valor booleano para deixar o if mais curto

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ const filme = {
 
 // Usando IF para checar se o filme é liberado para todas as idades
 if(filme.indicadoParaMaiores === false) {
-  console.log(`O filme ${filme.titulo} é indicado para todas as idades`)
+  console.log(`[if 01] O filme ${filme.titulo} é indicado para todas as idades`)
+}
+
+// Usando o mesmo IF mas... tirando proveito do valor booleano (truthy/falsy)
+// Se o valor é true e eu coloco uma exclamação antes, ele vira false.
+// Assim como, se o valor for false, com exclamação, ele vira true.
+// E se minha condição for simplesmente true, ela passa na validação. E vice-versa.
+if(!filme.indicadoParaMaiores) {
+  console.log(`[if 02] O filme ${filme.titulo} é indicado para todas as idades`)
 }
