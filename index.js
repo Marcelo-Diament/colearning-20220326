92225de53735579e72627c40b6ce688d75b16fcb
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-26 11:42:07 -0300

    feat: usando if padrão para checar idades permitidas

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const filme = {
   indicadoParaMaiores: false,
   ano: 2022
 }
+
+// Usando IF para checar se o filme é liberado para todas as idades
+if(filme.indicadoParaMaiores === false) {
+  console.log(`O filme ${filme.titulo} é indicado para todas as idades`)
+}
